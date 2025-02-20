02cecd8652574e411ccae7fa46c7363472908c6d
TipluJacob <j.ritterbach@tiplu.de>
2025-02-20 10:17:21 +0100

    fix: recording does not stop accidentally anymore

--- print_tracker_video_v2.py
+++ print_tracker_video_v2.py
@@ -10,7 +10,7 @@ import numpy as np
 x, y = 1820, 705 #Auswertung des Farbtons an dieser Position
 fps = 20
 folder_name = "Print_Tracker"
-range_red = list(chain(range(0, 21), range(340, 361))) # rote Farbwerte (HSV-Modell)
+range_red = list(chain(range(1, 11), range(340, 361))) # rote Farbwerte (HSV-Modell)
 thr = 7 #Threshold für die Anzahl der roten Pixel
 
 def is_usb_drive_mounted():
@@ -25,7 +25,7 @@ def is_usb_drive_mounted():
 def print_tracker():
     
     print_started = False
-    printer_homed = False # Um zu erkennen, dass ein Druck gestartet wird, muss sich der Drucker zuvor in der gehomten Position befinden
+    printer_homed = False # Um zu erkennen, dass ein Druck gestartet wird, muss sich der Drucker zuvor in der gehomten Position befunden haben
     n_mov = 0 # Anzahl der Bilder in denen eine Bewegung erkannt wurde
     n_not_mov = 0 # Anzahl der Bilder in denen keine Bewegung erkannt wurde
 
@@ -44,7 +44,6 @@ def print_tracker():
             red_pixel = 0 # Anzahl der roten Pixel in einem Bereich von 3x3 Pixeln
 
             mean_brightness = np.mean(img)
-            print(f"Mean brightness: {mean_brightness}")
 
             if mean_brightness >= 50:
 
@@ -59,13 +58,10 @@ def print_tracker():
                 if red_pixel < thr:
                     n_mov += 1
                     n_not_mov = 0
-                    print(f"Bewegung erkannt ({n_mov}x)")  
 
                 # Zählen der Bilder, in denen keine Bewegung erkannt wurde
                 elif red_pixel >= thr:
                     n_not_mov += 1
-                    n_mov = 0
-                    print(f"Keine Bewegung erkannt ({n_not_mov}x)")  
 
                 # Es wurde keine Bewegung erkannt und der Druck wurde noch nicht gestartet -> Das aktuelle Bild wird gelöscht
                 if red_pixel >= thr and print_started == False:
@@ -80,7 +76,7 @@ def print_tracker():
                 # Es wurde eine Bewegung in weniger als 5 Bildern hintereinander erkannt -> Es werden alle Bilder bis auf die letzten 4 gelöscht
                 elif red_pixel < thr and print_started == False and printer_homed == True and n_mov < 5:
                         img_file = sorted([f for f in os.listdir(folder_path) if f.endswith(('.PNG','.jpg'))])
-                        if len(img_file) >= 4:
+                        if len(img_file) >= 5:
                             img_file = img_file[:-4]
                             for file in img_file:
                                 os.remove(f"/media/robast/USB_Drive/imgs/{folder_name}/{file}")
@@ -92,14 +88,14 @@ def print_tracker():
                     print("Der Druck wurde gestartet.")
 
                 elif red_pixel < thr and print_started == True and printer_homed == True and n_mov >= 5:
-                    print("Der Druck läuft. Das Bild wird gespeichert.")
+                    n_not_mov = 0
 
                 # Es wurde keine Bewegung in mindestens 5 Bildern hintereinander erkannt -> Der Druck wurde beendet, das Video wird abgespeichert und die Bilder gelöscht
                 elif red_pixel >= thr and print_started == True and n_not_mov >= 5:
                     n_not_mov = 0
+                    n_mov = 0
                     print("Der Druck wurde beendet.")
                     img_file = sorted([f for f in os.listdir(folder_path) if f.endswith(('.PNG','.jpg'))])
-                    print(img_file)
                     first_img = cv2.imread(os.path.join(folder_path,img_file[0]))
                     height, width, _ = first_img.shape
                     video_writer = cv2.VideoWriter("/media/robast/USB_Drive/imgs/Zeitrafferaufnahme_" + current_time + ".avi", cv2.VideoWriter_fourcc(*'XVID'), fps, (width, height))
@@ -129,7 +125,6 @@ def print_tracker():
             with open("/home/robast/Robast/oak_server/error.log", "a") as f:
                 f.write(f"Error: {str(e)}\n")
             time.sleep(2)
-            print("Hier")
             continue
 
         time.sleep(5)
